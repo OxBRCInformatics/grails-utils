d6f36bd3d252d6cb48569d90fd6662b90e05674d
Ollie Freeman <oliver.freeman@ndm.ox.ac.uk>
2016-09-23 15:20:57 +0100

    Add the ability to convert databinding sources
    Allow nested DBS in convertion and binding by just accepting they are already DBS and not requiring
    conversion

--- utils/src/main/groovy/uk/ac/ox/ndm/grails/utils/databinding/OwnerFixGrailsWebDataBinder.groovy
+++ utils/src/main/groovy/uk/ac/ox/ndm/grails/utils/databinding/OwnerFixGrailsWebDataBinder.groovy
@@ -3,6 +3,8 @@ package uk.ac.ox.ndm.grails.utils.databinding
 import grails.core.GrailsApplication
 import grails.core.GrailsDomainClass
 import grails.databinding.DataBindingSource
+import grails.databinding.SimpleMapDataBindingSource
+import grails.databinding.converters.ValueConverter
 import grails.databinding.events.DataBindingListener
 import grails.web.databinding.GrailsWebDataBinder
 import groovy.transform.CompileStatic
@@ -39,5 +41,46 @@ class OwnerFixGrailsWebDataBinder extends GrailsWebDataBinder {
 
 
         return super.processProperty(obj, metaProperty, val, source, listener, errors)
+    }
+
+    @Override
+    protected Object convert(Class typeToConvertTo, Object value) {
+        if (value == null || typeToConvertTo.isAssignableFrom(value?.getClass())) {
+            return value
+        }
+        if (conversionHelpers.containsKey(typeToConvertTo)) {
+            ValueConverter converter = getConverter(typeToConvertTo, value)
+            if (converter) {
+                return converter.convert(value)
+            }
+        }
+        if (conversionService?.canConvert(value.getClass(), typeToConvertTo)) {
+            return conversionService.convert(value, typeToConvertTo)
+        }
+        if (Collection.isAssignableFrom(typeToConvertTo) && value instanceof String[]) {
+            if (Set == typeToConvertTo) {
+                return value as Set
+            }
+            if (List == typeToConvertTo) {
+                return value as List
+            }
+        }
+        else if (typeToConvertTo.isPrimitive() || typeToConvertTo.isArray()) {
+            return value
+        }
+        else if (value instanceof Map) {
+            def obj = typeToConvertTo.newInstance()
+            bind obj, new SimpleMapDataBindingSource(value)
+            return obj
+        }
+        else if (Enum.isAssignableFrom(typeToConvertTo) && value instanceof String) {
+            return convertStringToEnum(typeToConvertTo, value)
+        }
+        else if (value instanceof DataBindingSource) {
+            def obj = typeToConvertTo.newInstance()
+            bind obj, value as DataBindingSource
+            return obj
+        }
+        typeToConvertTo.newInstance value
     }
 }
