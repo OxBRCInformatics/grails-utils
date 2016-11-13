a5672be245261db280c15504cde66c195a022510
Ollie Freeman <oliver.freeman@ndm.ox.ac.uk>
2016-11-13 16:21:46 +0000

    Properly handle list pushdowns
    Pull lists back up after the pushdown

---
+++ utils/src/main/groovy/uk/ac/ox/ndm/grails/utils/databinding/PushedDownObject.groovy
@@ -0,0 +1,13 @@
+package uk.ac.ox.ndm.grails.utils.databinding
+
+/**
+ * @since 12/11/2016
+ */
+class PushedDownObject {
+
+    Object contents
+
+    PushedDownObject(Object contents) {
+        this.contents = contents
+    }
+}
