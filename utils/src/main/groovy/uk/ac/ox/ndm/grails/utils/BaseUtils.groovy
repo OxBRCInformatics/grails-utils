3e13db7c4e6cf076e08edc25346afe525452a783
Ollie Freeman <oliver.freeman@ndm.ox.ac.uk>
2017-11-22 09:15:25 +0000

    Update grails version and output field which error occurs on

--- utils/src/main/groovy/uk/ac/ox/ndm/grails/utils/BaseUtils.groovy
+++ utils/src/main/groovy/uk/ac/ox/ndm/grails/utils/BaseUtils.groovy
@@ -3,6 +3,7 @@ package uk.ac.ox.ndm.grails.utils
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.context.MessageSource
+import org.springframework.validation.FieldError
 
 /**
  * @since 20/09/2017
@@ -17,14 +18,14 @@ trait BaseUtils {
         logger.error 'Errors validating domain: {}', domainObj.class.simpleName
         System.err.println 'Errors validating domain: ' + domainObj.class.simpleName
         domainObj.errors.allErrors.each {error ->
-            if (messageSource) {
-                logger.error messageSource.getMessage(error, Locale.default)
-                System.err.println messageSource.getMessage(error, Locale.default)
-            }
-            else {
-                logger.error error.defaultMessage
-                logger.error "${Arrays.asList(error.arguments)}"
-            }
+
+            String msg = messageSource ? messageSource.getMessage(error, Locale.default) :
+                         "${error.defaultMessage} :: ${Arrays.asList(error.arguments)}"
+
+            if (error instanceof FieldError) msg += " [${error.field}]"
+
+            logger.error msg
+            System.err.println msg
         }
     }
 
@@ -43,6 +44,10 @@ trait BaseUtils {
     boolean checkAndSave(def domainObj, MessageSource messageSource) {
         if (!domainObj) return false
         if (!check(domainObj, messageSource)) return false
-        domainObj.save(failOnError: true) ? true : false
+        save(domainObj)
+    }
+
+    boolean save(def domainObj) {
+        domainObj.save(failOnError: true, validate: false, flush: true) ? true : false
     }
 }
